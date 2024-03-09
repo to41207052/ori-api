dd655ec2e07813522d5042edf3925aa3b445f4c9
to41207052 <to41207052@gmail.com>
2024-03-09 20:27:31 +0900

    POST 実装 Bodyの comment　のみを返す

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,22 +3,33 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+// テストスイートを定義
 describe('AppController (e2e)', () => {
+  // テストケースで使用する INestApplicationを宣言
   let app: INestApplication;
 
+  // AppModuleをインポートしてコンパイル
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    // nestアプリケーションを実体化してappに代入
     app = moduleFixture.createNestApplication();
+    // アプリケーション初期化
     await app.init();
   });
 
+  // it でテストケースを定義 ' ' は名前
   it('/ (GET)', () => {
+
+    // HTTPサーバーをリクエストするためのオブエクトを取得
     return request(app.getHttpServer())
+      // エンドポイントにリクエストを行う
       .get('/')
+      // ステータスコードが２００かどうか
       .expect(200)
+      // レスポンス本文が Hello World かどうか
       .expect('Hello World!');
   });
 });
