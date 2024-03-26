3b07a19b85e5e0509604665d90dd812bb129e79b
鈴木拓哉 <suzuki.t.hipster@gmail.com>
2024-03-26 12:17:27 +0900

    app.module typeORM関係消し忘れ

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,17 @@ import { AlbionModule } from './albion/albion.module';
   imports: [
     // CommentModule,
     AlbionModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT, 10) || 3306,
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_DATABASE || 'comment_db',
-      charset: 'utf8mb4',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST || 'localhost',
+    //   port: parseInt(process.env.DB_PORT, 10) || 3306,
+    //   username: process.env.DB_USERNAME || 'root',
+    //   password: process.env.DB_PASSWORD || 'root',
+    //   database: process.env.DB_DATABASE || 'comment_db',
+    //   charset: 'utf8mb4',
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [
